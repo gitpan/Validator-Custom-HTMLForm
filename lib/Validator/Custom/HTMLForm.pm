29462301117eeba1425e09b9782d426dca01c29c
Yuki Kimoto <kimoto.yuki@gmail.com>
2010-11-10 07:59:04 +0000

    Import of KIMOTO/Validator-Custom-HTMLForm-0.0611 from CPAN.
    gitpan-cpan-distribution: Validator-Custom-HTMLForm
    gitpan-cpan-version:      0.0611
    gitpan-cpan-path:         KIMOTO/Validator-Custom-HTMLForm-0.0611.tar.gz
    gitpan-cpan-author:       KIMOTO
    gitpan-cpan-maturity:     released

--- lib/Validator/Custom/HTMLForm.pm
+++ lib/Validator/Custom/HTMLForm.pm
@@ -1,5 +1,7 @@
 package Validator::Custom::HTMLForm;
 
+our $VERSION = '0.0611';
+
 use warnings;
 use strict;
 
@@ -7,27 +9,27 @@ use base 'Validator::Custom';
 
 use Validator::Custom::HTMLForm::Constraints;
 
-__PACKAGE__->register_constraint(
-    email             => \&Validator::Custom::HTMLForm::Constraints::email,
-    email_mx          => \&Validator::Custom::HTMLForm::Constraints::email_mx,
-    email_loose       => \&Validator::Custom::HTMLForm::Constraints::email_loose,
-    email_loose_mx    => \&Validator::Custom::HTMLForm::Constraints::email_loose_mx,
-    date              => \&Validator::Custom::HTMLForm::Constraints::date,
-    time              => \&Validator::Custom::HTMLForm::Constraints::time,
-    datetime          => \&Validator::Custom::HTMLForm::Constraints::datetime,
-    datetime_format   => \&Validator::Custom::HTMLForm::Constraints::datetime_format,
-    datetime_strptime => \&Validator::Custom::HTMLForm::Constraints::datetime_strptime,
-);
+sub new {
+    my $self = shift->SUPER::new(@_);
+    $self->register_constraint(
+        email             => \&Validator::Custom::HTMLForm::Constraints::email,
+        email_mx          => \&Validator::Custom::HTMLForm::Constraints::email_mx,
+        email_loose       => \&Validator::Custom::HTMLForm::Constraints::email_loose,
+        email_loose_mx    => \&Validator::Custom::HTMLForm::Constraints::email_loose_mx,
+        date              => \&Validator::Custom::HTMLForm::Constraints::date,
+        time              => \&Validator::Custom::HTMLForm::Constraints::time,
+        datetime          => \&Validator::Custom::HTMLForm::Constraints::datetime,
+        datetime_format   => \&Validator::Custom::HTMLForm::Constraints::datetime_format,
+        datetime_strptime => \&Validator::Custom::HTMLForm::Constraints::datetime_strptime,
+    );
+    return $self;
+}
 
 1;
 
 =head1 NAME
 
 Validator::Custom::HTMLForm - HTML Form Validator
-
-=cut
-
-our $VERSION = '0.0610';
 
 =head1 SYNOPSIS
 
